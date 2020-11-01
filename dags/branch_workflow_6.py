299010f5cb783c1f0b05764b6f630a01dc6cf454
Sankar <Sankar.penumacha@gmail.com>
2020-10-31 22:25:47 -0500

    branching in dag

--- dags/branch_workflow_6.py
+++ dags/branch_workflow_6.py
@@ -1,10 +1,9 @@
 """
 Concepts: Branching in workflow
 
-A simple dag with 2 tasks of type PythonOperators
-task_1 pushes {"task1_value": "Hello"} into xcom
-task_2 retrives task1_value (i.e. Hello ) from xcom
-adds string " World" to it and prints it
+A simple dag with a set of begin and end operators
+after begin branching happens to 5 different tasks
+randomly
 """
 
 from airflow.models import DAG
@@ -12,7 +11,7 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-import datetime
+from random import randrange
 
 default_args={
     'owner': 'sankar',
@@ -47,9 +46,7 @@ task_end = PythonOperator(
 )
 
 def branch_func(**context):
-    second = datetime.datetime.now().second
-    sec_mod_5 = str(second % 5)
-    return 'task_' + sec_mod_5
+    return 'task_' + str(randrange(5))
 
 task_branch = BranchPythonOperator(
     task_id='branching',
@@ -57,7 +54,7 @@ task_branch = BranchPythonOperator(
     python_callable=branch_func,
     provide_context=True
 )
-
+ 
 task_begin >> task_branch
 
 for task in range(5):
